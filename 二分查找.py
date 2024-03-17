a5138c0871ac4b6fe4f7713d0b374d8bfe87d4c7
zzb123456 <103801603+zzb2019053515@users.noreply.github.com>
2024-03-17 16:14:08 +0800

    Update 二分查找.py

--- 二分查找.py
+++ 二分查找.py
@@ -6,3 +6,17 @@ class Solution:
                 return i
         if (i == (len(nums)-1)):
             return -1
+##2.二分法
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left,right = 0,len(nums)-1
+        while left <= right:
+            mid  = (right - left)//2 + left
+            if (nums[mid] == target):
+                return mid
+            if (nums[mid] > target):
+                right = mid - 1
+            if  (nums[mid]< target):
+                left = mid + 1
+        if (left>right):
+            return -1
